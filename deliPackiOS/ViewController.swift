0c5b62c0cdd9189f0335a785e350aad6fecce880
Gh0stM0de <=jdotcole233@gmail.com>
2019-07-15 21:11:50 +0000

    first commit

---
+++ deliPackiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deliPackiOS
+//
+//  Created by Gh0stM0de on 7/15/19.
+//  Copyright © 2019 UltiTrust Company LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
